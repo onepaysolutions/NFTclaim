c52acc3a70f1c3cff755e3cc059af4c62602e983
onepaysolutions <admin@onepay.markets>
2024-11-20 17:01:21 +0800

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,1 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+export default defineConfig({
+  plugins: [react({ include: /\.(mdx|js|jsx|ts|tsx)$/ })],
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "./src"),
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
 
