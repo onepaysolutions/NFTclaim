24c7a18456dd5a8bec443764ecb659b1ca1f2a6d
onepaysolutions <admin@onepay.markets>
2024-11-20 17:07:37 +0800

    Add files via upload

---
+++ src/locales/zh.ts
@@ -0,0 +1,16 @@
+export default {
+  "Connect Wallet": "连接钱包",
+  "Switch to BSC": "切换到BSC",
+  "NFT Referral Program": "NFT 推荐计划",
+  "Earn rewards by sharing and collecting NFTs": "分享和收集 NFT 赚取奖励",
+  "Your NFTs": "你的 NFT",
+  "OPE Balance": "OPE 余额",
+  "Total Referrals": "总推荐人数",
+  "Connect your wallet to start": "连接钱包开始",
+  "Claim Your NFT": "领取你的 NFT",
+  "Get your exclusive NFT and start earning rewards": "获取专属 NFT 并开始赚取奖励",
+  "Claim NFT (1000 USDT)": "领取 NFT (1000 USDT)",
+  "Your Referral Link": "你的推荐链接",
+  "Copy": "复制",
+  "Referral link copied!": "推荐链接已复制！"
+};
